c21b75dea98b1a7741d048556779887b317343ac
kandirou0 <simo.kandirou15@gmail.com>
2023-01-16 22:33:20 +0100

    update

--- CureCo/app/views/index.php
+++ CureCo/app/views/index.php
@@ -1,8 +1,51 @@
 <?php require_once 'inc/header.php'  ?>
 <?php require_once 'inc/navbar.php'  ?>
 
-<div>
-    home page
-</div>
+
+<section class="bg-white dark:bg-gray-900">
+    <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
+        <div class="font-light text-gray-500 sm:text-lg dark:text-gray-400">
+            <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white capitalize">discover
+                the best cruise ships</h2>
+            <p class="mb-4">There are so many reasons to get excited about a ShipCruiseTour® cruise. Choose from
+                itineraries that take you to top-rated vacation destinations, from sun-soaked Caribbean isles to ancient
+                temples in Asia and everywhere in between. Make the most out of every moment at sea while traveling
+                onboard the world’s best cruise ships, loaded from bow to stern with game-changing thrills, dazzling
+                entertainment, as well as next-level dining and unmatched activities for all kinds of adventure-seekers.
+                No matter which ship you choose to sail on, this is a fleet filled with incredible feats.</p>
+        </div>
+        <div class="grid grid-cols-2 gap-4 mt-8">
+            <img class="w-full rounded-lg"
+                 src="https://images.pexels.com/photos/11820071/pexels-photo-11820071.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                 alt="office content 1">
+            <img class="mt-4 w-full lg:mt-10 rounded-lg"
+                 src="https://images.pexels.com/photos/1122462/pexels-photo-1122462.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                 alt="office content 2">
+        </div>
+    </div>
+</section>
+
+
+<section class="bg-white dark:bg-gray-900">
+    <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
+        <div class="font-light text-gray-500 sm:text-lg dark:text-gray-400 order-2">
+            <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white capitalize">There's
+                more to explore</h2>
+            <p class="mb-4">If you’re ready to
+                learn more about the incredible adventures you can have on ShipCruiseTour, check out our 2021-2022
+                sailings — they’re all open to book right now. Dive into our deck plans and become an expert on the
+                fleet. Or discover why our Oasis Class and Amplified ships are rated among the best cruise ships in the
+                world</p>
+        </div>
+        <div class="grid grid-cols-2 gap-4 mt-8 lg:order-1">
+            <img class="w-full rounded-lg"
+                 src="https://images.pexels.com/photos/2634269/pexels-photo-2634269.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                 alt="office content 1">
+            <img class="mt-4 w-full lg:mt-10 rounded-lg"
+                 src="https://images.pexels.com/photos/2634269/pexels-photo-2634269.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                 alt="office content 2">
+        </div>
+    </div>
+</section>
 
 <?php require_once 'inc/footer.php'  ?>
