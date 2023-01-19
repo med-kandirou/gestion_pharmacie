148e728aea1b729b4d3c9ea362fa83f0fec95ccb
kandirou0 <simo.kandirou15@gmail.com>
2023-01-19 21:45:52 +0100

    update

--- CureCo/app/views/admin/gestion.php
+++ CureCo/app/views/admin/gestion.php
@@ -175,19 +175,22 @@
                 <form class="space-y-6">
                     <div>
                         <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nom du produit :</label>
-                        <input type="text" id="nom_prod" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" >
+                        <input type="text" id="nom_prod" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white">
+                        <p class="text-red-600 hidden" id="nom_prod_err">Le nom est obligatoire</p>
                     </div>
                     <div>
                         <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Quantité :</label>
-                        <input type="number" id="quantite" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" >
+                        <input type="number" id="quantite" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white">
+                        <p class="text-red-600 hidden" id="quantite_err">La quantité est obligatoire</p>
                     </div>
                     <div>
                         <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Prix :</label>
                         <input type="number" id="prix" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white">
+                        <p class="text-red-600 hidden" id="prix_err">Le prix est obligatoire</p>
                     </div>
                     <label class="block text-sm font-medium text-gray-900 dark:text-white">Selectionner une catégorie</label>
                     <select class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                        <option id="category">Choose a country</option>
+                        <option id="category"></option>
                         <?php foreach($data2 as $cat): ?>
                             <option value="<?= $cat['id_cat'] ?>"><?= $cat['cat'] ?></option>
                         <?php endforeach; ?>
@@ -196,7 +199,7 @@
                         <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="multiple_files">Choisir une image</label>
                         <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="multiple_files" type="file" multiple>
                     </div>
-                    <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Ajouter</button>
+                    <button type="button" id="update" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Update</button>
                 </form>
             </div>
         </div>
--- CureCo/public/js/gestion.js
+++ CureCo/public/js/gestion.js
@@ -19,17 +19,44 @@ $(document).ready(function () {
             $.post("../Products/getinfo/", {id_prod:item.value},
                 function (response) {
                     var data=JSON.parse(response);
-                    console.log(data);
                     $('#nom_prod').val(data.libelle);
                     $('#quantite').val(data.quantite);
                     $('#prix').val(data.prix);
                     $('#category').val(data.id_cat)
                     $('#category').text(data.cat);
-
                 },
             );
         });
     });
+    // update products
+    function getempty(field){
+        if($('#'+field+'').val()==''){
+            document.getElementById(''+field+'_err').classList.remove('hidden');
+            return 1;
+        }
+        else{
+            document.getElementById(''+field+'_err').classList.add('hidden');
+            return 0;
+        }
+    }
+
+    $('#update').click(function () { 
+        getempty('nom_prod');
+        getempty('quantite');
+        getempty('prix');
+
+        if(getempty('nom_prod')==0 && getempty('quantite')==0 && getempty('prix')==0 ){
+            $.post("../Products/update", {nom:$('nom_prod').val() , quantite:$('quantite').val() ,prix:$('prix').val()},
+                function (data) {
+                    
+                },
+            );
+        }
+
+        
+    });
+
+
 });
 
 
