7a5f3b15c8c024663ce1b20a1bde856d8498c7df
kandirou0 <simo.kandirou15@gmail.com>
2023-01-23 22:22:41 +0100

    update

--- CureCo/app/views/admin/addProduct.php
+++ CureCo/app/views/admin/addProduct.php
@@ -49,9 +49,6 @@
                     </div>
                     <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Ajouter</button>
                 </form></br>
-                <div id="alert" class="hidden p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
-                    <span class="font-medium">alert!</span> Il faut saisir tous les champs !!
-                </div>
             </div>
         </div>
     </div>
