c05a1691afb9bd0a550aca15065dfc14c71f55a7
kandirou0 <simo.kandirou15@gmail.com>
2023-01-25 16:34:00 +0100

    update

--- CureCo/app/views/index.php
+++ CureCo/app/views/index.php
@@ -4,7 +4,7 @@
 <section class="bg-white dark:bg-gray-900">
     <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div class="mr-auto place-self-center lg:col-span-7">
-            <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Payments tool for software companies</h1>
+            <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Health Care</h1>
             <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">From checkout to global sales tax compliance, companies around the world use Flowbite to simplify their payment stack.</p>
             <a href="#" class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base f text-center text-white rounded-lg bg-blue-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
                 Get started
@@ -24,7 +24,7 @@
     <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
         <div class="text-gray-500 sm:text-lg dark:text-gray-400">
             <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white capitalize">discover
-                the best cruise ships</h2>
+                Health with us</h2>
             <p class="mb-4">There are so many reasons to get excited about a ShipCruiseTour® cruise. Choose from
                 itineraries that take you to top-rated vacation destinations, from sun-soaked Caribbean isles to ancient
                 temples in Asia and everywhere in between. Make the most out of every moment at sea while traveling
